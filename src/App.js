461a29ca725c0aa41de696ba568c6b4d2c7557ac
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-12 02:04:43 +0100

    Add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div>
-      <h1>Let's build a e-commerce app</h1>
+      <Home />
     </div>
   );
 }

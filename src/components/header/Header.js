e58a5743036e220b05fa692981b5b52214e7a99c
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-17 23:11:21 +0100

    Add cartIcon and cartDropDown components, implement cart reducer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 import Logo from "../../assets/Logo.png";
 import { auth } from "../../firebase/Firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/CartIcon";
+import CartDropDown from "../cart-dropdown/CartDropDown";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -27,12 +29,15 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropDown />}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const CartActionTypes = {
+  TOGGLE_CART_HİDDEN: "TOGGLE_CART_HİDDEN",
+};
+
+export default CartActionTypes;

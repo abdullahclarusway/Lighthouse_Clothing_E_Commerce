dd0a68b37c5ef68630f3b2cb4331d424c06e6b49
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-03-18 02:16:11 +0100

    Update shop data, update cart utils

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,4 +3,9 @@ import CartActionTypes from "./cart.types";
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HİDDEN,
     
+});
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
 })
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
   TOGGLE_CART_HİDDEN: "TOGGLE_CART_HİDDEN",
+  ADD_ITEM: "ADD_ITEM",
 };
 
 export default CartActionTypes;
